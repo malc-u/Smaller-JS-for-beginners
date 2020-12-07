b52d6c0fb9c27db8031ce29245a86aa660d398f0
MalC-U <mwcalow@gmail.com>
2020-12-07 13:28:52 +0000

    Added 24 hrs clock

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,6 +1,8 @@
 const clockDisplay = document.getElementById("clock");
+const clockTwoDisplay = document.getElementById("clocktwo");
 let date;
-let timeNow; 
+let timeNow;
+let timeTwoNow; 
 
 let clock = setInterval(getTime, 1000);
 
@@ -11,4 +13,21 @@ function getTime(){
 };
 
 
+function showTime(){
+    date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
 
+    hours = (hours < 10 ) ? "0" + hours : hours;
+    minutes = (minutes < 10 ) ? "0" + minutes : minutes;
+    seconds = (seconds < 10 ) ? "0" + seconds : seconds;
+
+    timeTwoNow = hours + " : " + minutes + " : " + seconds;
+
+    clockTwoDisplay.innerHTML = timeTwoNow;
+
+    setInterval(showTime, 1000)
+}
+
+showTime();
